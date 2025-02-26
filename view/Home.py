20e2d869c5466b4c402974eb5bf832423b1e9b35
Chlwilliams <Chlwilliams98@gmail.com>
2025-02-25 18:21:26 -0600

    Basic format to GUI
    Will continue to make it look nicer / add more functionalities.

---
+++ view/Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+def run():
+    st.set_page_config(
+        page_title="Test",
+        page_icon="",
+    )
+
+    st.title("VulGPT")
+    st.sidebar.success("Select a page")
+
+    st.write("""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum.""")
+    
+
+if __name__ == "__main__":
+    run()
+
