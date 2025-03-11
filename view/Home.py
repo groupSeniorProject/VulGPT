5001b63f3943f3da80db90c8054160f84aed6d61
TelloCS <jeremytello31@gmail.com>
2025-03-10 20:07:59 -0500

    Updated GUI with pie chart

--- view/Home.py
+++ view/Home.py
@@ -1,24 +1,40 @@
+import os
+import pandas as pd
 import streamlit as st
+import plotly.express as px
+from dotenv import load_dotenv
+from neo4j import GraphDatabase
+# from langchain_community.graphs import Neo4jGraph
+# import matplotlib.pyplot as plt
 
+load_dotenv(override=True)
+
+uri = os.getenv("URI")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+driver = GraphDatabase.driver(uri=uri, auth=(username, password))
+
+def execute_query(query):
+    with driver.session() as session:
+        result = session.run(query)
+        records = result.data()
+        return pd.DataFrame(records)
 
 def run():
-    st.set_page_config(
-        page_title="Test",
-        page_icon="",
-    )
-
     st.title("VulGPT")
-    st.sidebar.success("Select a page")
 
-    st.write("""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-            officia deserunt mollit anim id est laborum.""")
+    if driver is not None:
+        query = execute_query("MATCH (n:Vulnerability) RETURN n.package_name AS package_name")
+        df = pd.DataFrame(query)
+
+        package_name_count = df['package_name'].value_counts()
+        fig_pie = px.pie(package_name_count,
+                         values=package_name_count.values,
+                         names=package_name_count.index,
+                         title="Pie Chart")
+        st.plotly_chart(fig_pie)
+
     
-
 if __name__ == "__main__":
     run()
-
