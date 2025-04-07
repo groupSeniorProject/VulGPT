82503350294c4243918f4fea0655aa47d1736cfc
jlpj032 <jpliego03+git@gmail.com>
2025-04-07 15:55:23 -0500

    Add files via upload
    Added new features and methods that can be used, such as getting commit hashes from versions tags, a full repo language breakdown and a smaller version language breakdown, as well as full repo size calculation and local language breakdown. also added methods for making shallow copies of repositories with where we only download the information needed for the specific versions. It's much faster than cloning the repository and is also much smaller.

--- neo4j_scripts/process_git_cve.py
+++ neo4j_scripts/process_git_cve.py
@@ -1,42 +1,130 @@
 import glob
 import json
+import os 
+import subprocess
+import requests
+import time
+
+personal_key = "YOUR_GITHUB_API_KEY_HERE"
+
+headers = {
+	'Authorization' : f'token {personal_key}'
+}
 
 def read_json_file(file_path):
-    # Opens json filea nd loads it, and returns.
-    with open(file_path, "r", encoding="utf-8") as file:
-        return json.load(file)
+	# Opens json filea nd loads it, and returns.
+	with open(file_path, "r", encoding="utf-8") as file:
+		return json.load(file)
 
 def write_json_file(file_path, data):
-    # writes json files at a specified location
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+	# writes json files at a specified location
+	with open(file_path, 'w') as file:
+		json.dump(data, file, indent=4)
+
+def get_repo_name_from_url(url):
+	# Here we are using the url gathered to extract the repo name so we can check if it exist
+	return url.strip("/").split("/")[-2:]
+
+def get_repo_size(repo: list):
+	if '.git' in repo[1]:
+		repo[1] = repo[1][:-4]
+	# here we will just get a json file containing a bunch of information about the repo
+	api_url = "https://api.github.com/repos/{}/{}".format(repo[0], repo[1])
+	response = requests.get(api_url, headers=headers)
+	# because of api rate limits will sleep for 1 second 
+	time.sleep(0.73)
+	if response.status_code == 200:
+		#This is the size given in kB
+		size = response.json()['size']
+		return size
+	# to many request
+	elif response.status_code == 429:
+		print("(size) I'm going to sleep, see you in an hour") 
+		# request is limited by the hour 
+		time.sleep(60)
+		# try again after an hour
+		print("(size) I'm awake now")
+		get_repo_languages(repo)
+	else:
+		print(f"(size) could not get information for repo {repo[0]}/{repo[1]}, code : {response.status_code}")
+		return None
+
+def get_lang_sum(data):
+	total = 0
+	for lang in data.keys():
+		total = total + data[lang]
+	return total
+
+def get_lang_percent(data, total_sum):
+	out = { }
+	for lang in data.keys():
+		# I'm doing it as 2 seperate statements to hopefully reduce floating point errors
+		div = data[lang] / total_sum
+		per = round(div * 100, 10)
+		out[lang] = per
+	return out
+
+def get_repo_languages(repo: list):
+	if '.git' in repo[1]:
+		repo[1] = repo[1][:-4]
+	api_url = "https://api.github.com/repos/{}/{}/languages".format(repo[0], repo[1])
+	response = requests.get(api_url, headers=headers)
+	# good response
+	if response.status_code == 200:
+		data = response.json()
+		return data
+	# to many request
+	elif response.status_code == 429 or response.status_code == 403:
+		print("(lang) I'm going to sleep, see you in an minute")
+		# request is limited by the hour 
+		time.sleep(60)
+		# try again after an hour
+		print("(lang) I'm awake now")
+		get_repo_languages(repo)
+	else:
+		print(f"(lang) could not get information for repo {repo[0]}/{repo[1]}, code : {response.status_code}")
+		return None
 
 def get_CVE_files():
-    data_path = "/mnt/disk-5/osv_data/all_data/*CVE*.json"
-    json_data_files = glob.glob(data_path)
-    return json_data_files
+	data_path = "/mnt/disk-5/osv_data/all_data/*CVE*.json"
+	json_data_files = glob.glob(data_path)
+	return json_data_files
 
-cve_files = get_CVE_files()
-print(len(cve_files))
+if __name__ == '__main__':
+	cve_files = get_CVE_files()
+	print(len(cve_files))
 
-# I'm going to use a dictionaries to save the found data since this can easily be converted into a json file
-found_data = { }
-notFound = 0
+	# I'm going to use a dictionaries to save the found data since this can easily be converted into a json file
+	found_data = { }
+	notFound = 0
 
-# this is used to find the data
-for file in range(len(cve_files)):
-    try: 
-        osv_data = read_json_file(cve_files[file])
-        data = { } 
-        # from what I've tested the "repo" information is found on the last item in the array, so thats where we are looking at
-        for d in osv_data['affected'][len(osv_data['affected']) - 1]['ranges']:
-            if d['repo'] not in found_data.keys():
-                found_data[d['repo']] = []	
-            found_data[d['repo']].append({ osv_data['id'] : d['events']}) 
-    except Exception as e:
-        print(f"error has occured: {e}")
-        notFound = notFound + 1
+	# this is used to find the data
+	for file in range(len(cve_files)):
+		try: 
+			osv_data = read_json_file(cve_files[file])
+			data = { } 
+			# from what I've seen the "repo" information is found on the last item in the array, so thats where we are looking at
+			for d in osv_data['affected'][len(osv_data['affected']) - 1]['ranges']:
+				if d['repo'] not in found_data.keys():
+					#since this is the first time calculating we will need to add this info
+					lang_data = get_repo_languages(get_repo_name_from_url(d['repo']))
+					if lang_data is None:
+						lang_per = "Could not get"
+					else:
+						lang_per = get_lang_percent(lang_data, get_lang_sum(lang_data))
+					found_data[d['repo']] = {
+							'lang-breakdown' :  lang_per,
+							'size' : get_repo_size(get_repo_name_from_url(d['repo']))
+						} 
+				found_data[d['repo']][osv_data['id']] = d['events']
 
-# here we can makee the found data into a json file. Possible using this for loading the data without having to run the program again
-write_json_file("found_data.json", found_data)
-print(notFound)
+
+					
+		except Exception as e:
+			if 'repo' not in str(e): 
+				print(f"an error occured: {e}")
+			notFound = notFound + 1
+
+	# here we can makee the found data into a json file. Possible using this for loading the data without having to run the program again
+	write_json_file("found_data.json", found_data)
+	print(notFound)
