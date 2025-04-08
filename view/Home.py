6ca1e95f59501b77896a3d6d0a3e46244a39701c
TelloCS <jeremytello31@gmail.com>
2025-04-08 13:26:16 -0500

    added pie chart

--- view/Home.py
+++ view/Home.py
@@ -4,8 +4,6 @@ import streamlit as st
 import plotly.express as px
 from dotenv import load_dotenv
 from neo4j import GraphDatabase
-# from langchain_community.graphs import Neo4jGraph
-# import matplotlib.pyplot as plt
 
 load_dotenv(override=True)
 
@@ -29,10 +27,56 @@ def get_total_node_vulns(query, total_nodes: str):
         result = session.run(query)
         return result.single()[total_nodes]
 
-def search_results(df, text_search: str):
-    m1 = df["Ecosystems"].str.contains(text_search)
+@st.cache_data
+def unique_eco_pie_chart(query, limit):
+    with driver.session() as session:
+        result = session.run(query)
+        records = result.data()
+        eco_df = pd.DataFrame(records)
+        eco_counts = eco_df['clean_name'].value_counts().reset_index().head(limit)
+        eco_counts.columns = ['Ecosystem', 'Count']
+
+        # Pie chart
+        fig = px.pie(
+            eco_counts,
+            names='Ecosystem',
+            values='Count',
+            title=f'{limit} Ecosystem Name Distribution',
+            color_discrete_sequence=px.colors.sequential.RdBu,
+            hover_data=['Count']
+        )
+
+        fig.update_traces(
+            textinfo='none',
+            textposition='outside',
+            pull=[0.03] * len(eco_counts),
+            marker=dict(line=dict(color='#000000', width=1)),
+            hovertemplate='<b>%{label}</b><br>Count: %{value}<br>Percent: %{percent}<extra></extra>'
+        )
+
+        fig.update_layout(
+            showlegend=True,
+            legend_title_text='Ecosystem',
+            margin=dict(t=40, b=20, l=20, r=20),
+            height=750,
+            paper_bgcolor="rgba(0,0,0,0)",
+            plot_bgcolor="rgba(0,0,0,0)"
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+def search_results(df: pd.DataFrame, text_search: str):
+    m1 = df["Ecosystems"].str.lower().str.contains(text_search.lower())
     df_search = df[m1]
     return df_search
+
+def previous_next_page_buttons():
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.button('Previous Page')
+    with col2:
+        st.button('Next Page')
 
 def run():
     st.title("VulGPT")
@@ -45,27 +89,17 @@ def run():
 
         text_search = st.text_input("Search Vulnerabilities")
 
-        query = execute_query("MATCH (n:Ecosystem) RETURN n.ecosystem_name AS name", 'Ecosystems')
-        st.write(query)
+        query = execute_query(f"MATCH (n:Ecosystem) RETURN n.ecosystem_name AS name LIMIT 10", 'Ecosystems')
 
-        # MATCH (n:Ecosystem) WHERE n.ecosystem_name = "PyPI" RETURN COUNT(n) -> 1
+        limit = st.selectbox("Select number of ecosystems to display:", [5, 10, 15, 20, 25], index=4)
+
+        unique_eco_pie_chart("MATCH (n:Ecosystem) RETURN split(n.ecosystem_name, ':')[0] AS clean_name", limit)
+
+        # previous_next_page_buttons()
 
         if text_search:
             st.write(search_results(query, text_search))
 
-        
-
-        # package_name = execute_query("MATCH (n:Vulnerability) RETURN n.package_name AS package_name")
-        # df = pd.DataFrame(query)
-        # df_pn = pd.DataFrame(package_name)
-        # st.write(f"Total: {df['Vulnerability'].count()}")
-
-        # package_name_count = df_pn['package_name'].value_counts()
-        # fig_pie = px.pie(package_name_count,
-        #                  values=package_name_count.values,
-        #                  names=package_name_count.index,
-        #                  title="Pie Chart")
-        # st.plotly_chart(fig_pie)
 
     
 if __name__ == "__main__":
